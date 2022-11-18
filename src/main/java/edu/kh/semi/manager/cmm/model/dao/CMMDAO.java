cdd6a1a274a5f045cc914b545817078a15263ac9
rlagpwls9795 <rlagpwls9795@naver.com>
2022-11-19 04:40:23 +0900

    게시판 관리

--- src/main/java/edu/kh/semi/manager/cmm/model/dao/CMMDAO.java
+++ src/main/java/edu/kh/semi/manager/cmm/model/dao/CMMDAO.java
@@ -1,5 +1,13 @@
 package edu.kh.semi.manager.cmm.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class CMMDAO {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
 
 }
