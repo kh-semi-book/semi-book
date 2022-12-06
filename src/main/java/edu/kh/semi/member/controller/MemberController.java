638ebd6bfef904ad55898e750d96074127c0114c
jeonghyun <jhyeon310@naver.com>
2022-12-06 19:45:02 +0900

    로그인시 알림, 사소한 css 수정

--- src/main/java/edu/kh/semi/member/controller/MemberController.java
+++ src/main/java/edu/kh/semi/member/controller/MemberController.java
@@ -45,6 +45,7 @@ public class MemberController {
 			if(loginMember.getAuthority()==2) {
 				path="/manager/selectBook";
 			} else {
+				ra.addFlashAttribute("message", loginMember.getMemberName()+"("+loginMember.getMemberId()+")님 환영합니다!");
 				
 				path = "/";
 				
@@ -66,7 +67,7 @@ public class MemberController {
 		} else {
 			path = referer; // 이전페이지로 이동
 			// model.addAttribute("message","회원 아이디 또는 비밀번호가 일치하지 않습니다.");
-			ra.addFlashAttribute("message", "아이디 또는 비밀번호가 일치하지 않습니다.");
+			ra.addFlashAttribute("message", "회원 아이디 또는 비밀번호가 일치하지 않습니다.\n\n 확인 후 다시 시도하시기 바랍니다.");
 		}
 
 		return "redirect:" + path;
